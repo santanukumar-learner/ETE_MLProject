8f487328ccdb74d57ca2221765e28a911acf4e53
santanukumar-learner <santanukumaradhikari07@gmail.com>
2024-11-04 19:19:41 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-# from setuptools import find_packages, setup
-# from typing import List
-
-# hypen='-e .'
-
-# def get_requrements(file_path:str) -> List[str]:
-#     requirements=[]
-#     with open(file_path) as file_obj:
-#         requirements=file_obj.readlines()
-#         [req.replace('\n', " ") for req in requirements]
-#         if hyper in requiremets:
-#             requirements.remove(hyper)
-#     print(requirements)
-
-# setup(
-#     name='mlproject',
-#     version='0.0.1',
-#     author='santanu',
-#     author_email="santanukumaradhikari07@gmail.com",
-#     packages=find_packages(),
-#     install_requires=get_requirements('REQUIREMENTS.txt')
-
-# )
-
 from setuptools import find_packages, setup
 from typing import List
 
