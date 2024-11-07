3bb875429dc35ce85a08249408396df329d6145a
santanukumar-learner <santanukumaradhikari07@gmail.com>
2024-11-07 21:49:42 +0530

    model trainer

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -6,7 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from data_transformation import Datatransformation
-from data_transformation import Datatransformationconfig
+# from data_transformation import Datatransformationconfig
+from model_trainer import ModelTrainer
 
 
 @dataclass
@@ -48,4 +49,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = Datatransformation()
-    data_transformation.initiate_datatransformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_datatransformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/Components/data_transformation.py
+++ src/Components/data_transformation.py
@@ -17,39 +17,40 @@ class Datatransformationconfig:
     preprocessor_obj_file_path = os.path.join("artifacts", "preprocessor.pkl")
 
 
-class Datatransformation:
-    def __init__(self):
-        self.data_transformation_config = Datatransformationconfig()
-
-    def get_data_transformer_object(self):
-        try:
-            numerical_columns = ['writing_score', 'reading_score']
-            categorical_feature = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch',
-                                   'test_preparation_course']
-            num_pipeline = Pipeline(
+def get_data_transformer_object():
+    try:
+        numerical_columns = ['writing_score', 'reading_score']
+        categorical_feature = ['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch',
+                               'test_preparation_course']
+        num_pipeline = Pipeline(
             steps=[
                 ("imputer", SimpleImputer(strategy="median")),
                 ("scaler", StandardScaler())
             ]
-            )
-            cat_pipeline = Pipeline(
-                steps=[
-                    ("imputer", SimpleImputer(strategy="mode")),
-                    ("onehotencoder", OneHotEncoder())
-                ]
-            )
-            logging.info("numerical encoding completed")
-            logging.info("categorical encoding completed")
-            preprocessor = ColumnTransformer(
-                [
-                    ("num_pipeline", num_pipeline, numerical_columns),
-                    ("categorical_pipeline", cat_pipeline, categorical_feature)
-                ]
-            )
-            return preprocessor
+        )
+        cat_pipeline = Pipeline(
+            steps=[
+                ("imputer", SimpleImputer(strategy="mode")),
+                ("onehotencoder", OneHotEncoder())
+            ]
+        )
+        logging.info("numerical encoding completed")
+        logging.info("categorical encoding completed")
+        preprocessor = ColumnTransformer(
+            [
+                ("num_pipeline", num_pipeline, numerical_columns),
+                ("categorical_pipeline", cat_pipeline, categorical_feature)
+            ]
+        )
+        return preprocessor
 
-        except Exception as e:
-            raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+class Datatransformation:
+    def __init__(self):
+        self.data_transformation_config = Datatransformationconfig()
 
     def initiate_datatransformation(self, train_path, test_path):
 
@@ -57,28 +58,34 @@ class Datatransformation:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
             logging.info("Reading training and test dat complete")
+
             logging.info("Obtaining preprocessor object")
-            preprocessing_obj = self.get_data_transformer_object()
-            numerical_column = ['writing_score', 'reading_score']
+            preprocessing_obj = get_data_transformer_object()
+
+            # numerical_column = ['writing_score', 'reading_score']
             target_column = "math_score"
+
             input_feature_train_df = train_df.drop(columns=[target_column])
             target_feature_train_df = train_df[target_column]
+
             input_feature_test_df = test_df.drop(columns=[target_column])
             target_feature_test_df = test_df[target_column]
+
             logging.info(
                 f"Applying preprocessing object on train and test dataset"
             )
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_test_df)
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
 
             logging.info(f"Saved preprocessing object")
+
             save_object(
-                file_path = self.data_transformation_config.preprocessor_obj_file_path,
-                obj = preprocessing_obj
-            )
+                file_path=self.data_transformation_config.preprocessor_obj_file_path, obj=preprocessing_obj)
+
             return (train_arr,
                     test_arr,
                     self.data_transformation_config.preprocessor_obj_file_path)
